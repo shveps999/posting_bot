d62bd0749048abc4e8424d730c8b3eafbb6e0acc
shveps999 <privetzdarovahappy@gmail.com>
2025-09-02 23:11:28 +0300

    Update notification_keyboard.py

--- events_bot/bot/keyboards/notification_keyboard.py
+++ events_bot/bot/keyboards/notification_keyboard.py
@@ -8,7 +8,7 @@ def get_post_notification_keyboard(
     builder = InlineKeyboardBuilder()
     builder.button(text="❤️ В избранное", callback_data=f"notify_like_{post_id}")
     if url:
-        builder.button(text="🔗 Открыть ссылку", url=url)
+        builder.button(text="🔗 Подробнее", url=url)
     builder.button(text="🏠 Главное меню", callback_data="main_menu")
     builder.adjust(2)
     return builder.as_markup()
