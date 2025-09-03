2127813da622a1c7cf656f0befa288208d2ed534
shveps999 <privetzdarovahappy@gmail.com>
2025-09-03 21:07:45 +0300

    Update moderation_handlers.py

--- events_bot/bot/handlers/moderation_handlers.py
+++ events_bot/bot/handlers/moderation_handlers.py
@@ -156,12 +156,12 @@ async def process_moderation_action(callback: CallbackQuery, state: FSMContext, 
             try:
                 await callback.bot.send_message(
                     chat_id=post.author_id,
-                    text=f"Ваше мероприятие '{post.title}' одобрено и опубликовано 🤟🙂‍↔️",
+                    text=f"Ваше мероприятие «{post.title}» одобрено и опубликовано 🤟😌",
                 )
             except Exception:
                 pass
 
-            await callback.answer("Ваше мероприятие одобрено и опубликовано 🤟🙂‍↔️")
+            await callback.answer("Ваше мероприятие одобрено и опубликовано 🤟😌")
             await callback.message.delete()
         else:
             logfire.error(f"Ошибка при одобрении поста {post_id}")
@@ -203,7 +203,7 @@ async def receive_moderator_comment(message: Message, state: FSMContext, db):
             try:
                 await message.bot.send_message(
                     chat_id=post.author_id,
-                    text=f"Ваше мероприятие '{post.title}' отклонено 🫠\n\n Комментарий модератора: {comment}",
+                    text=f"Ваше мероприятие «{post.title}» отклонено 🥲\n\n Комментарий модератора: {comment}",
                 )
             except Exception:
                 pass
@@ -221,7 +221,7 @@ async def receive_moderator_comment(message: Message, state: FSMContext, db):
             try:
                 await message.bot.send_message(
                     chat_id=post.author_id,
-                    text=f"Ваше мероприятие '{post.title}' требует изменений ✍️🧐\n\n Комментарий модератора: {comment}",
+                    text=f"Ваше мероприятие «{post.title}» требует изменений ✍️🧐\n\n Комментарий модератора: {comment}",
                 )
             except Exception:
                 pass
