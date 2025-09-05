16a3b977e450f0a521a31c66180efaa370d7418a
shveps999 <privetzdarovahappy@gmail.com>
2025-09-05 14:33:33 +0300

    Update __init__.py

--- events_bot/bot/__init__.py
+++ events_bot/bot/__init__.py
@@ -3,6 +3,8 @@ from .handlers import (
     register_user_handlers,
     register_post_handlers,
     register_callback_handlers,
+    register_moderation_handlers,
+    register_feed_handlers,
 )
 from .keyboards import (
     get_main_keyboard,
@@ -11,7 +13,6 @@ from .keyboards import (
     get_category_selection_keyboard,
 )
 from .states import UserStates, PostStates
-from .utils import send_post_notification
 from .middleware import DatabaseMiddleware
 
 __all__ = [
@@ -19,12 +20,13 @@ __all__ = [
     "register_user_handlers",
     "register_post_handlers",
     "register_callback_handlers",
+    "register_moderation_handlers",
+    "register_feed_handlers",
     "get_main_keyboard",
     "get_city_keyboard",
     "get_category_keyboard",
     "get_category_selection_keyboard",
     "UserStates",
     "PostStates",
-    "send_post_notification",
     "DatabaseMiddleware",
 ]
