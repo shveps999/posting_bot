f77aadab3cf9762719b17f15bc88291471cc6f9a
shveps999 <privetzdarovahappy@gmail.com>
2025-09-06 21:17:30 +0300

    Update user_states.py

--- events_bot/bot/states/user_states.py
+++ events_bot/bot/states/user_states.py
@@ -3,6 +3,10 @@ from aiogram.fsm.state import State, StatesGroup
 
 class UserStates(StatesGroup):
     """Состояния для регистрации пользователя"""
-
     waiting_for_city = State()
     waiting_for_categories = State()
+
+
+class NavigationState(StatesGroup):
+    """Состояние для отслеживания текущего раздела"""
+    current_section = State()  # 'feed', 'liked', 'menu'
