b4fc524e85a1b3c4c5d7d98209d4d4a62601be53
shveps999 <privetzdarovahappy@gmail.com>
2025-09-10 16:11:03 +0300

    Update user_states.py

--- events_bot/bot/states/user_states.py
+++ events_bot/bot/states/user_states.py
@@ -4,7 +4,6 @@ from aiogram.fsm.state import State, StatesGroup
 class UserStates(StatesGroup):
     """Состояния для регистрации пользователя"""
     waiting_for_city = State()
-    waiting_for_cities = State()  # Добавлено новое состояние
     waiting_for_categories = State()
 
 
