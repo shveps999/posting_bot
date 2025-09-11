759d26d405c6f5169d753988df2f4a206c4d8e45
shveps999 <privetzdarovahappy@gmail.com>
2025-09-11 15:05:08 +0300

    Update main_keyboard.py

--- events_bot/bot/keyboards/main_keyboard.py
+++ events_bot/bot/keyboards/main_keyboard.py
@@ -5,12 +5,12 @@ def get_main_keyboard() -> InlineKeyboardMarkup:
     """Главное меню с кнопкой избранного"""
     builder = InlineKeyboardBuilder()
     
-    builder.button(text="📮 Актуальные мероприятия", callback_data="feed")
-    builder.button(text="❤️ Мое избранное⠀⠀⠀⠀⠀⠀⠀", callback_data="liked_posts")
-    builder.button(text="🔖 Создать мероприятие⠀⠀", callback_data="create_post") 
-    builder.button(text="⭐️ Изменить категории⠀⠀⠀", callback_data="change_category")
-    builder.button(text="🎓 Изменить университет⠀", callback_data="change_city")
-    builder.button(text="💬 Помощь⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", callback_data="help")
+    builder.button(text="📮⠀⠀Актуальные мероприятия", callback_data="feed")
+    builder.button(text="❤️⠀⠀Мое избранное⠀⠀⠀⠀⠀⠀⠀", callback_data="liked_posts")
+    builder.button(text="🔖⠀⠀Создать мероприятие⠀⠀", callback_data="create_post") 
+    builder.button(text="⭐️⠀⠀Изменить категории⠀⠀⠀", callback_data="change_category")
+    builder.button(text="🎓⠀⠀Изменить университет⠀", callback_data="change_city")
+    builder.button(text="💬⠀⠀Помощь⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", callback_data="help")
     
     builder.adjust(1)
     return builder.as_markup()
