4868dc71a8b7e0091d0198ffed02cf0335a690f0
shveps999 <privetzdarovahappy@gmail.com>
2025-09-11 15:43:38 +0300

    Update main_keyboard.py

--- events_bot/bot/keyboards/main_keyboard.py
+++ events_bot/bot/keyboards/main_keyboard.py
@@ -7,10 +7,10 @@ def get_main_keyboard() -> InlineKeyboardMarkup:
     
     builder.button(text="📮⠀Актуальные мероприятия", callback_data="feed")
     builder.button(text="❤️⠀Мое избранное⠀⠀⠀⠀⠀⠀⠀", callback_data="liked_posts")
-    builder.button(text="✏️⠀Создать мероприятие⠀⠀", callback_data="create_post") 
+    builder.button(text="🔖⠀Создать мероприятие⠀⠀", callback_data="create_post") 
     builder.button(text="⭐️⠀Изменить категории⠀⠀⠀", callback_data="change_category")
     builder.button(text="🎓⠀Изменить университет⠀", callback_data="change_city")
-    builder.button(text="💬⠀Помощь⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", callback_data="help")
+    builder.button(text="🗯⠀Помощь⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", callback_data="help")
     
     builder.adjust(1)
     return builder.as_markup()
