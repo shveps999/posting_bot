c126070c2987992754f9a46fd6386812563082c1
shveps999 <privetzdarovahappy@gmail.com>
2025-09-26 01:21:23 +0300

    Update city_repository.py

--- events_bot/database/repositories/city_repository.py
+++ events_bot/database/repositories/city_repository.py
@@ -9,21 +9,33 @@ class CityRepository:
 
     @staticmethod
     async def get_all_active(db: AsyncSession) -> List[City]:
-        result = await db.execute(select(City).where(City.is_active == True))
+        """Получить все активные города, отсортированные по возрастанию ID"""
+        result = await db.execute(
+            select(City)
+            .where(City.is_active == True)
+            .order_by(City.id.asc())
+        )
         return result.scalars().all()
 
     @staticmethod
     async def get_by_id(db: AsyncSession, city_id: int) -> Optional[City]:
+        """Получить город по ID"""
         result = await db.execute(select(City).where(City.id == city_id))
         return result.scalar_one_or_none()
 
     @staticmethod
     async def get_by_ids(db: AsyncSession, city_ids: List[int]) -> List[City]:
-        result = await db.execute(select(City).where(City.id.in_(city_ids)))
+        """Получить города по списку ID (сохраняет порядок из запроса)"""
+        result = await db.execute(
+            select(City)
+            .where(City.id.in_(city_ids))
+            .order_by(City.id.asc())  # Опционально: сортировка по ID
+        )
         return result.scalars().all()
 
     @staticmethod
     async def create_city(db: AsyncSession, name: str) -> City:
+        """Создать новый город"""
         city = City(name=name)
         db.add(city)
         await db.commit()
