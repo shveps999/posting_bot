1db53d4fb70223dcf207d426e5ca628643d82783
shveps999 <privetzdarovahappy@gmail.com>
2025-09-26 03:17:45 +0300

    Update moderation_handlers.py

--- events_bot/bot/handlers/moderation_handlers.py
+++ events_bot/bot/handlers/moderation_handlers.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 import logfire
+import os
 from events_bot.database.services import (
     ModerationService,
     PostService,
